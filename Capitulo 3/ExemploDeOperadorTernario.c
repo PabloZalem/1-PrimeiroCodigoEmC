5b809c32e484505129bee783a286e56993c91655
Pablo Zalem <zalem.pablo@yahoo.com>
2026-02-23 21:09:56 -0300

    Ternario

---
+++ Capitulo 3/ExemploDeOperadorTernario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+    int y=0, x=0;
+
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+
+    printf("Digite o valor de y: ");
+    scanf("%d", &y);
+
+    printf("\n\n");
+
+    y=x<=0 ? x+1 : x-1;
+    printf("O resultado de y: %d\n", y);
+
+    system("pause");
+    system("cls");
+
+    return 0;
+}
