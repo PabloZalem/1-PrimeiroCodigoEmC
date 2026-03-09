66e84c86b36801645847cb9c3876605fa2bd0222
Pablo Zalem <zalem.pablo@yahoo.com>
2026-03-09 20:19:42 -0300

    Construindo um exemplo completo usando DO-WHILE

---
+++ Capitulo 3/ExemploComplentoDoDoWhile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+    int tipo = 0;
+
+    printf("\n*** R E C E B E N D O  D A D O S ***\n");
+
+    do {
+
+    } while(tipo != 1 && tipo != 2);
+
+    system("pause");
+    system("cls");
+
+    return 0;
+}
