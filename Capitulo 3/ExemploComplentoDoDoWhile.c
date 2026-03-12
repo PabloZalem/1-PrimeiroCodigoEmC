20c61038466ae013953dfdb34133aa4778a91d98
Pablo Zalem <zalem.pablo@yahoo.com>
2026-03-12 20:33:39 -0300

    Continuação da construcao da aplicação

--- Capitulo 3/ExemploComplentoDoDoWhile.c
+++ Capitulo 3/ExemploComplentoDoDoWhile.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <conio.h>
 
-int main() {
+int main()
+{
     int tipo = 0;
 
     printf("\n*** R E C E B E N D O  D A D O S ***\n");
 
-    do {
+    do
+    {
+        printf("\n\n Informe o tipo de venda 1-Varejista ou 2-Atacado: ");
+        scanf("%d", &tipo);
+        if (tipo != 1 && tipo != 2)
+        {
+            printf("\n\n Tipo informado invalido!!!");
+        }
 
-    } while(tipo != 1 && tipo != 2);
+    } while (tipo != 1 && tipo != 2);
 
     system("pause");
     system("cls");
